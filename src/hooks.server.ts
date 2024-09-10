f5583b964542d29aff34ebcd0b3e8419fed63438
cailen <103433837+cailenxyvid@users.noreply.github.com>
2024-09-10 13:14:51 -0400

    Build pipeline setup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,11 @@
-export { handle } from "./auth"
+import type { HandleServerError } from '@sveltejs/kit';
+
+// authjs magic interception
+export { handle } from "$lib/auth/auth"
+
+// https://kit.svelte.dev/docs/hooks#shared-hooks-handleerror
+export const handleError: HandleServerError = async ({ error, event, status, message }) => {
+    //todo: integrate New Relic here
+	console.error('Unhandled server side error!', status, message, error, event);
+	return { message: 'Fatal Error: ' + message, status: status };
+};
