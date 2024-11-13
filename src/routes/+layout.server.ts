3ff844472de4861e1a83f9d087a14a3ad6b3b567
cailen <103433837+cailenxyvid@users.noreply.github.com>
2024-11-13 15:19:34 -0500

    .

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,14 @@
 import type { LayoutServerLoad } from "./$types"
  
 export const load: LayoutServerLoad = async (event) => {
-  const session = await event.locals.auth()
+  console.log('checking session')
+  let session = null;
+  try {
+    session = await event.locals.auth()
+  console.log('session', session)
+  } catch (e:any) {
+    console.log('session error', e.message ?? e)
+  }
   // const session = {}
  
   return {
